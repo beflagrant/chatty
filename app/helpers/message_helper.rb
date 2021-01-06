dde8a54170338dddbfa406c3d61d6f9aaa524a6c
Jonathan Greenberg <jonathan.s.greenberg@gmail.com>
2021-01-06 13:55:04 -0500

    Only allow user to edit their own messages
    - reverse flex column direction for autoscrolling from bottom of
      messages

--- app/helpers/message_helper.rb
+++ app/helpers/message_helper.rb
@@ -1,10 +1,14 @@
 module MessageHelper
-  def message_color(message, for_messenger)
-    is_messenger = (for_messenger.nil? ? is_messenger?(message) : for_messenger)
-    is_messenger ? "bg-sky text-blue-900" : "bg-tan text-yellow-900"
+  def message_color(message, locals)
+    for_messenger?(message, locals) ? "bg-sky text-blue-900" : "bg-tan text-yellow-900"
   end
 
   def is_messenger?(message)
     message.user == current_user
+  end
+
+  def for_messenger?(message, locals)
+    for_messenger = locals[:for_messenger]
+    for_messenger.nil? ? is_messenger?(message) : for_messenger
   end
 end
