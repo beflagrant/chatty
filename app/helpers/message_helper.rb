a85c44b62263a1eb0530a9d55bc1b6450ac748d4
Kelly Rauwerdink <kelly.rauwerdink@gmail.com>
2021-01-08 11:23:28 -0600

    Enhance current chat app

--- app/helpers/message_helper.rb
+++ app/helpers/message_helper.rb
@@ -1,5 +1,5 @@
 module MessageHelper
   def message_color(message)
-    message.user == current_user ? "bg-sky text-blue-900" : "bg-tan text-yellow-900"
+    message.user == current_user ? "bg-sky text-blue-900 self-end align-right" : "bg-purple text-indigo-900"
   end
 end
